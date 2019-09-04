cdaf66bda632c65e4dba5d1bc46c09c80c9bde12
Thomas <thomas.lintanf@efrei.net>
2019-09-04 14:11:57 +0200

    Defined chat and update method toString for chien

--- src/zoo/Chat.java
+++ src/zoo/Chat.java
@@ -1,5 +1,60 @@
 package zoo;
 
-public class Chat extends Animal{
+/**
+ * 
+ * @author Thomas LINTANF
+ *
+ */
 
+public class Chat extends Animal{
+	private String race;
+	
+	public Chat(int poids, String nom, String race) {
+		super(poids,nom);
+		this.race = race;
+	}
+
+	/**
+	 * @return the race
+	 */
+	public String getRace() {
+		return race;
+	}
+
+	/**
+	 * @param race the race to set
+	 */
+	public void setRace(String race) {
+		this.race = race;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((race == null) ? 0 : race.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chat other = (Chat) obj;
+		if (race == null) {
+			if (other.race != null)
+				return false;
+		} else if (!race.equals(other.race))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Chat "  + this.getNom() + " de race=" + race + " et de poids " + this.getPoids();
+	}
 }
